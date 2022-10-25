2f0665346bea4759e21bb54e0725ec850e0455d7
abdullahsyedz <abdullah.syed1971@gmail.com>
2022-10-24 22:33:53 -0400

    edited app.py

--- app.py
+++ app.py
@@ -9,19 +9,21 @@ mongo_client = pymongo.MongoClient("mongo")
 database1 = mongo_client["312Project"]  # username database
 username_table = database1["usernames"]  # username collection
 
+def sanitizeText(text):
+    if(not isinstance(text, str)):
+        return
+    return text.replace('&', '&#38;').replace('<', '&#60;').replace('>', '&#62;')
+
+def validateText(text):
+    return text and len(text.strip()) != 0
 @app.route('/', methods=['POST', 'GET'])
 def hello_world():  # put application's code here for login page
     if flask.request.method == 'POST':
-        formUsername = flask.request.form['uname']
+        formUsername = sanitizeText(flask.request.form['uname'])
         formPassword = flask.request.form['psw']
-        formUsername = formUsername.replace('&', '&#38;')
-        formUsername = formUsername.replace('<', '&#60;')
-        formUsername = formUsername.replace('>', '&#62;')
+
         desiredEntry = list(username_table.find({"username": formUsername}))
         if len(desiredEntry) == 0:  # username is not in the database
-            # userInfo = {"username": formUsername, "password": formPassword}
-            # username_table.insert_one(userInfo)
-            # return render_template("profile.html", User=formUsername)
             warning1 = "username not valid"
             return render_template("index.html", loginStatus=warning1)
         else:
@@ -34,6 +36,23 @@ def hello_world():  # put application's code here for login page
     else:
         noWarning = ""
         return render_template("index.html", loginStatus=noWarning)
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if flask.request.method == 'POST':
+        formUsername = sanitizeText(flask.request.form['uname'])
+        formPassword = flask.request.form['psw']
+        if(not validateText(formUsername) and not validateText(formPassword)): #check to make sure its not empty or null
+            return render_template("signup.html", signUpStatus="Invalid input")
+
+        desiredEntry = list(username_table.find({"username": formUsername}))
+        if len(desiredEntry) != 0:
+            return render_template("signup.html", signUpStatus="Username already exists")
+
+        username_table.insert_one({"username": formUsername, "password": formPassword})
+        return render_template("profile.html", User=formUsername)
+    else:
+        return render_template("signup.html", signUpStatus="")
 
 
 if __name__ == '__main__':
